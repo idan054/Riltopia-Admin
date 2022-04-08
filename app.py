85afd0945e6aa9e5a121351ead5e35838e6a71d3
Idan054 <idanbit80@gmail.com>
2022-04-08 16:38:28 +0300

    Deploy GET stream token api, with hidden secret

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import stream_chat
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+# current date and time
+now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+load_dotenv()
+STREAM_API_SECRET = os.getenv('STREAM_API_SECRET')
+
 app = Flask(__name__)
+@app.route('/firebase', methods=["GET", "POST"])
+def home():
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    # example: http://127.0.0.1:5000/firebase?uid=325245&name=idan
+    print('Start:')
+    args = request.args
+    uid = args.get('uid')
+    name = args.get('name')
+    print(f'{uid} - {name}')
 
+    server_client = stream_chat\
+        .StreamChat(api_key="ah48ckptkjvm",
+                    api_secret=f"{STREAM_API_SECRET}")
+
+    token = server_client.create_token(f'{uid}')
+    print(f'token {token}')
+
+    # return render_template("base.html")
+    return {
+        'get_api_call_at' : f'{now}',
+        'firebase_uid' : f'{uid}',
+        'firebase_name' : f'{name}',
+        'getStream_token' : f'{token}'
+    }
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True)
